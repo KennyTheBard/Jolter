fa1ad50c4483beab1773a71c194605366bd51515
KennyTheBard <octav_teculescu@yahoo.com>
2021-08-03 14:40:55 +0300

    Add file size and ignores already converted files

--- batch_import.sh
+++ batch_import.sh
@@ -3,8 +3,22 @@
 py_script="$JOLTER_PATH/import_obj.py"
 
 IFS=$'\n'
-for objfile in $(find -type f -name "*.obj")
+objfiles=$(find -type f -name "*.obj")
+total=$(find -type f -name "*.obj" -printf \\n | wc -l)
+
+index=$((1))
+for objfile in $objfiles
 do
-    echo blender -P "${py_script}" -b "$(readlink -f ${objfile})"
-    blender -P "${py_script}" -b "$(readlink -f ${objfile})"
+    filepath=$(readlink -f ${objfile})
+    blendfile="${filepath%.*}.blend"
+
+    if [ -e $blendfile ]
+    then
+        echo "Progress: $index/$total skipped... $(du -h $blendfile | cut -f1)"
+    else
+        blender -P "$py_script" -b "$filepath" > /dev/null 2>&1
+        echo "Progress: $index/$total completed... $(du -h $blendfile | cut -f1)"
+    fi
+
+    index=$((index+1))
 done
